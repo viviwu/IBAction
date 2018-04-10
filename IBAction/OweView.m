4a3026bc73c42979be8cb6583c7cd825edfac34d
vivi wu <vivi705@qq.com>
2018-04-10 12:21:22 +0800

    Add
    Add

---
+++ IBAction/OweView.m
@@ -0,0 +1,24 @@
+//
+//  OweView.m
+//  IBAction
+//
+//  Created by csco on 2018/4/10.
+//  Copyright © 2018年 csco. All rights reserved.
+//
+
+#import "OweView.h"
+
+@implementation OweView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (IBAction)helloVivi:(id)sender {
+    NSLog(@"123:%s", __func__);
+}
+
+@end
--- IBAction/ViewController.m
+++ IBAction/ViewController.m
@@ -9,9 +9,11 @@
 #import "ViewController.h"
 #import "XWView.h"
 #import "WXView.h"
+#import "OweView.h"
 
 @interface ViewController ()
 @property (strong, nonatomic) IBOutlet XWView *myView;
+@property (strong, nonatomic) WXView * codeView;
 
 @end
 
@@ -26,11 +28,20 @@
     NSLog(@"DD++++%s", __func__);
 }
 
+- (IBAction)helloVivi:(id)sender {
+    NSLog(@"456:%s", __func__);
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    WXView * view = [[NSBundle mainBundle]loadNibNamed:@"WXView" owner:self options:nil].lastObject;
-    [view setCenter:self.view.center];
+    self.codeView = [[NSBundle mainBundle]loadNibNamed:@"WXView" owner:self options:nil].lastObject;
+    [self.codeView setCenter:self.view.center];
+    [self.view addSubview:self.codeView];
+ 
+    OweView * view = [[NSBundle mainBundle]loadNibNamed:@"OweView" owner:self options:nil].lastObject;
+    [view setCenter:CGPointMake(self.view.center.x, self.view.center.y+200)];
     [self.view addSubview:view];
     // Do any additional setup after loading the view, typically from a nib.
 }
